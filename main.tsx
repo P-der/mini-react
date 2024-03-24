4d1603f17caa50b6b721001fccb6bf319c29b0de
P-der <1198811801@qq.com>
2024-03-24 21:03:30 +0800

    02 任务调度器demo

--- main.tsx
+++ main.tsx
@@ -7,3 +7,15 @@
 import ReactDom from "./core/react-dom"
 import App from './app.jsx'
 ReactDom.createRoot(document.getElementById('root')).render(App)
+let task =1
+function workflow(deadline) {
+    let showBreak = false
+    while(!showBreak) {
+        task ++
+        showBreak = deadline.timeRemaining() < 1
+        console.log('task', 1)
+    }
+    console.log(2222)
+    requestIdleCallback(workflow)
+}
+requestIdleCallback(workflow)
